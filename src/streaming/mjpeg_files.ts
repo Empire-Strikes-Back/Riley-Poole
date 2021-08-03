e913f0058f25c422cca1a72495921d15a1d22cb1
streaming <streaming@program>
2021-08-03 16:18:50 +0300

    i remove hls typescript src files

--- src/streaming/mjpeg_files.ts
+++
@@ -1,3 +0,0 @@
-import * as fs from 'fs-extra'
-import * as path from 'path'
-import { logger } from './_logger'
--- src/streaming/ui_comp_content.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-import styled, { injectGlobal, ThemeProvider, THEME, withTheme } from 'src/modules/styled-components'
-import { Layout } from 'antd'
-export const NavLinkStyled = styled(Layout.Content) `
-
-`;
